93019e722f135d01d94fee9dc6f85b8f1b8833ff
azizahika <azizah.ika.wulandari-2022@vokasi.unair.ac.id>
2023-09-25 13:46:06 +0700

    Create 2023_09_18_015808_ulasan.php

---
+++ database/migrations/2023_09_18_015808_ulasan.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ulasan', function (Blueprint $table) {
+            $table->id('id_ulasan')->increments();
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id_user')->on('Users');
+            $table->string('deskripsi_ulasan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+         Schema::dropIfexists('ulasan');
+    }
+};
