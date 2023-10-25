95639e15b77bc0001b1173b3f388e901ad247831
riizzz <rizkiriandi1803@gmail.com>
2023-10-25 20:33:21 +0700

    Create navbar.blade.php

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,37 @@
+
+@section('navbar')
+<div class="header_section">
+         <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+               <a class="navbar-brand"href="index.html"><img src="images/logo.png"></a>
+               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+               <span class="navbar-toggler-icon"></span>
+               </button>
+               <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav ml-auto">
+                     <li class="nav-item">
+                        <a class="nav-link" href="index">Home</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="about">About</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="services">Services</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="gallery">Vehicles</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="client">Client</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="contact">Contact</a>
+                     </li>
+                  </ul>
+                  <form class="form-inline my-2 my-lg-0">
+                  </form>
+               </div>
+            </nav>
+         </div>
+      </div>
+@endsection
